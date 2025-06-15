9a8afc2f7970eacd47a309ccfac054cf9623d995
Anil <nanil6304@gmail.com>
2025-06-15 12:45:09 +0530

    fifth commit

--- calculator.py
+++ calculator.py
@@ -1,13 +1,16 @@
-def total(x,y):
-    return x+y 
+def total(x, y):
+    return x + y
 
-def difference(x,y):
-    return x-y
 
-def multiply(x,y):
-    return x*y
+def difference(x, y):
+    return x - y
 
-def divide(x,y):
-    if y==0:
-        raise ValueError('Cannot divide by Zero')
-    return x/y 
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    if y == 0:
+        raise ValueError("Cannot divide by Zero")
+    return x / y
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,18 +1,23 @@
 from calculator import total, difference, multiply, divide
-import pytest 
+import pytest
+
 
 def test_add():
-    assert total(3,2)==5
-    
+    assert total(3, 2) == 5
+
+
 def test_subtract():
-    assert difference(10,4)==6
+    assert difference(10, 4) == 6
+
 
 def test_multiply():
-    assert multiply(8,5)==40
-    
+    assert multiply(8, 5) == 40
+
+
 def test_divide():
-    assert divide(40,5)==8
-    
+    assert divide(40, 5) == 8
+
+
 def test_divide_by_zero():
     with pytest.raises(ValueError):
-        divide(5,0)
+        divide(5, 0)
