1bc353c22e9438ac93bf8f28ac26ef4f938f3bba
Anil <nanil6304@gmail.com>
2025-06-15 14:11:13 +0530

    10 tailed beast

--- tests/test_train_model.py
+++ tests/test_train_model.py
@@ -1,5 +1,7 @@
 import os
 from train_model import train_and_save_model
+
+
 def test_train_and_save_model():
     train_and_save_model()
     assert os.path.exists("model.joblib")
--- train_model.py
+++ train_model.py
@@ -2,16 +2,16 @@ import joblib
 from sklearn.datasets import load_diabetes
 from sklearn.linear_model import LinearRegression
 
+
 def train_and_save_model():
     data = load_diabetes()
-    x,y = data.data, data.target 
-    
+    x, y = data.data, data.target
+
     model = LinearRegression()
-    model.fit(x,y)
-    
+    model.fit(x, y)
+
     joblib.dump(model, "model.joblib")
-    
-    
+
+
 if __name__ == "__main__":
     train_and_save_model()
-    
