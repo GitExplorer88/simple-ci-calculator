f4e1ae8e897dfd94f1b3b4b6da54114eac7a9812
Anil <nanil6304@gmail.com>
2025-06-15 11:13:20 +0530

    My first CI setup

---
+++ calculator.py
@@ -0,0 +1,13 @@
+def total(x,y):
+    return x+y 
+
+def difference(x,y):
+    return x-y
+
+def multiply(x,y):
+    return x*y
+
+def divide(x,y):
+    if y==0:
+        raise ValueError('Cannot divide by Zero')
+    return x/y 
